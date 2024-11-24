0aa906b916e3422f6f170afc9eb647fffda737b9
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-11-24 16:55:59 +0000

    feat(plugins): wotaku threads plugin

---
+++ src/plugins/wotaku.ts
@@ -0,0 +1,22 @@
+import { definePlugin } from '#framework'
+
+const SUBMIT_CHANNEL_ID = '1143161987291947178'
+const LINK_RE =
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi
+
+export default definePlugin({
+  name: 'wotaku',
+  onLoad: (client) => {
+    client.on('messageCreate', async (message) => {
+      if (
+        message.channelID === SUBMIT_CHANNEL_ID &&
+        !message.author.bot &&
+        message.content.match(LINK_RE) !== null
+      ) {
+        await message.startThread({
+          name: 'Discussion 🧵'
+        })
+      }
+    })
+  }
+})
