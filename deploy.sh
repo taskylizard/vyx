105fa333f6371eca609a92b3563eb9feee11b2ab
taskylizard <75871323+taskylizard@users.noreply.github.com>
2025-09-29 20:32:18 +0000

    deploy cli refining

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 WITH_SEED=false
+WITH_MIGRATE=false
 
 while [[ $# -gt 0 ]]; do
-  case $1 in
-    --with-seed)
-      WITH_SEED=true
-      shift
-      ;;
-    *)
-      echo "Unknown option: $1"
-      exit 1
-      ;;
-  esac
+   case $1 in
+     --seed)
+       WITH_SEED=true
+       shift
+       ;;
+     --migrate)
+       WITH_MIGRATE=true
+       shift
+       ;;
+     *)
+       echo "Unknown option: $1"
+       exit 1
+       ;;
+   esac
 done
 
 export INFISICAL_TOKEN_SERVICE=$(infisical login --method=universal-auth --client-id=$INFISICAL_MACHINE_CLIENT_ID --client-secret=$INFISICAL_MACHINE_CLIENT_SECRET --silent --plain)
 
+if [ "$WITH_MIGRATE" = "true" ]; then
+   export RUN_MIGRATE=true
+fi
+
 if [ "$WITH_SEED" = "true" ]; then
-  export RUN_SEED_AND_MIGRATE=true
+   export RUN_SEED=true
 fi
 
 docker compose up -d --build --remove-orphans
